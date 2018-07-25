f21a1f4f87d8f1f890b77a251a735ddd80d9a516
Yoga Budi Yulianto <yogabudiyulianto@gmail.com>
2018-07-25 20:49:00 +0700

    Upload semua source code Java

---
+++ src/filemanager/FileManager.java
@@ -0,0 +1,73 @@
+package filemanager;
+
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserCommandEvent;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserEvent;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.EventQueue;
+import javax.swing.SwingUtilities;
+
+import filemanager.webviewui.*;
+
+/**
+ *
+ * @author Yoga Budi Yulianto
+ */
+public class FileManager extends JFrame
+implements PendengarWebBrowser {
+
+  public WebViewUI ui;
+
+  public FileManager() {
+    super();
+
+    this.setSize(1200, 650);
+    this.setTitle("Yoga File Manager");
+    this.setName("UI Utama");
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.ketengahkan();
+//    this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+    ui = new WebViewUI(this);
+    ui.setPendengarWebBrowser(this);
+    ui.setURL("file:///home/ini_laptop/NetBeansProjects/finex-filemanager-ui/public_html/index.html");
+//    ui.setURL("https://stephanwagner.me/jBox/");
+
+    this.getContentPane().add(ui);
+  }
+
+  public void ketengahkan() {
+    Dimension dimensiLayar = getToolkit().getScreenSize();
+    int x = (int) (dimensiLayar.getWidth() - this.getWidth()) / 2;
+    int y = (int) (dimensiLayar.getHeight() - this.getHeight()) / 2;
+
+    this.setLocation(x, y);
+  }
+
+  public static void main(String[] args) {
+    WebViewUI.inisialisasi();
+    
+    EventQueue.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new FileManager().setVisible(true);
+      }
+    });
+    
+    WebViewUI.jalankanEventPump();
+  }
+
+  @Override
+  public void saatSelesaiLoading(WebBrowserEvent wbe, JWebBrowser browser) {
+    
+  }
+
+  @Override
+  public void saatPerintahDiterima(WebBrowserCommandEvent wbce, JWebBrowser browser) {
+    
+  }
+
+}
---
+++ src/filemanager/webviewui/WebViewUI.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filemanager.webviewui;
+
+import chrriis.common.UIUtils;
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserCommandEvent;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserEvent;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserListener;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserNavigationEvent;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserWindowOpeningEvent;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserWindowWillOpenEvent;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Yoga Budi Yulianto
+ */
+public class WebViewUI extends JPanel
+        implements WebBrowserListener {
+
+  private JFrame windowUtama;
+  private JWebBrowser browser;
+  private PendengarWebBrowser pendengarWeb;
+
+  public WebViewUI(JFrame windowUtama) {
+    this.windowUtama = windowUtama;
+    this.pendengarWeb = null;
+    
+    this.browser = new JWebBrowser();
+    this.browser.setMenuBarVisible(false);
+    this.browser.setBarsVisible(false);
+    this.browser.setButtonBarVisible(false);
+    this.browser.setJavascriptEnabled(true);
+    this.browser.addWebBrowserListener(this);
+
+    this.setLayout(new BorderLayout());
+    this.add(browser, BorderLayout.CENTER);
+  }
+
+  public JWebBrowser dapatkanKomponenUtama() {
+    return browser;
+  }
+  
+  public void setPendengarWebBrowser(PendengarWebBrowser pdg) {
+    this.pendengarWeb = pdg;
+  }
+  
+  public void setURL(String url) {
+    this.browser.navigate(url);
+  }
+  
+  public void eksekusiJavascript(String js) {
+    this.browser.executeJavascript(js);
+  }
+  
+  public Object eksekusiJavascriptDenganHasil(String js) {
+    return this.browser.executeJavascriptWithResult(js);
+  }
+
+  public static void inisialisasi() {
+    NativeInterface.open();
+    UIUtils.setPreferredLookAndFeel();
+  }
+
+  public static void jalankanEventPump() {
+    NativeInterface.runEventPump();
+  }
+
+  @Override
+  public void windowWillOpen(WebBrowserWindowWillOpenEvent wbwwoe) {
+  }
+
+  @Override
+  public void windowOpening(WebBrowserWindowOpeningEvent wbwoe) {
+  }
+
+  @Override
+  public void windowClosing(WebBrowserEvent wbe) {
+  }
+
+  @Override
+  public void locationChanging(WebBrowserNavigationEvent wbne) {
+  }
+
+  @Override
+  public void locationChanged(WebBrowserNavigationEvent wbne) {
+  }
+
+  @Override
+  public void locationChangeCanceled(WebBrowserNavigationEvent wbne) {
+  }
+
+  @Override
+  public void loadingProgressChanged(WebBrowserEvent wbe) {
+    JWebBrowser webBrowser = wbe.getWebBrowser();
+    
+    if(this.pendengarWeb != null) {
+      if(webBrowser.getLoadingProgress() >= 100) {
+        this.pendengarWeb.saatSelesaiLoading(wbe, webBrowser);
+      }
+    }
+  }
+
+  @Override
+  public void titleChanged(WebBrowserEvent wbe) {
+  }
+
+  @Override
+  public void statusChanged(WebBrowserEvent wbe) {
+  }
+
+  @Override
+  public void commandReceived(WebBrowserCommandEvent wbce) {
+    JWebBrowser webBrowser = wbce.getWebBrowser();
+    
+    if(this.pendengarWeb != null) {
+      this.pendengarWeb.saatPerintahDiterima(wbce, webBrowser);
+    }
+  }
+
+}
