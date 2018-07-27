fae3838678710a1f6644df91f532350ca8478528
Yoga Budi Yulianto <yogabudiyulianto@gmail.com>
2018-07-27 14:20:31 +0700

    update class BreadcrumbBerkas
    penambahan enkapsulasi dan desain ulang

--- src/ui/js/BreadcrumbBerkas.js
+++ src/ui/js/BreadcrumbBerkas.js
@@ -2,17 +2,35 @@
 class BreadcrumbBerkas extends Berkas {
   
   constructor(nama) {
-    super("folder", nama);
-  }
-  
-  tandai() {
-    var id = this.nama.split(" ").join("");
-    $("#bc_" + id + " .yby-breadcrumb").addClass("yby-breadcrumb-aktif");
-  }
-  
-  hilangkanTanda() {
-    var id = this.nama.split(" ").join("");
-    $("#bc_" + id + " .yby-breadcrumb").removeClass("yby-breadcrumb-aktif");
+    super();
+    this.setNama(nama);
+    this.setJenis("folder");
+    
+    this.tandai = function() {
+      var id = this.getNama().split(" ").join("");
+      $("#bc_" + id + " .yby-breadcrumb").addClass("yby-breadcrumb-aktif");
+    };
+    
+    this.hilangkanTanda = function() {
+      var id = this.getNama().split(" ").join("");
+      $("#bc_" + id + " .yby-breadcrumb").removeClass("yby-breadcrumb-aktif");
+    };
+    
+    this.pasangElemen = function(elemenTempat) {
+      var id = this.getNama().split(" ").join("");
+    
+      var bc =
+        "<div id='bc_"+id+"' "+
+          "class='col breadFolder' path-absolut='"+this.getPathAbsolut()+"'>"+
+          "<div class='card hoverable yby-breadcrumb'>"+
+            "<div class='yby-breadcrumb-elemen center-align'>"+
+              this.getNama() +
+            "</div>"+
+          "</div>"+
+        "</div>";
+
+      elemenTempat.append(bc);
+    };
   }
   
   static warnaiBreadcrumb() {
@@ -34,32 +52,16 @@ class BreadcrumbBerkas extends Berkas {
       $(this).addClass("white");
     });
   }
-  
-  pasangElemen(elemenTempat) {
-    var id = this.nama.split(" ").join("");
-    
-    var bc =
-      "<div id='bc_"+id+"' "+
-        "class='col breadFolder' path-absolut='"+this.pathAbsolut+"'>"+
-        "<div class='card hoverable yby-breadcrumb'>"+
-          "<div class='yby-breadcrumb-elemen center-align'>"+
-            this.nama +
-          "</div>"+
-        "</div>"+
-      "</div>";
-      
-    elemenTempat.append(bc);
-  }
 };
 
 // testing
 $(document).ready(function() {
   var bc = new BreadcrumbBerkas("ini bukan folder");
-  bc.pathAbsolut = "/home/folder" + bc.nama;
+  bc.setPathAbsolut("/home/folder" + bc.getNama());
   bc.pasangElemen($("#panelBreadcrumb"));
   
   var bc2 = new BreadcrumbBerkas("ini bc 2");
-  bc2.pathAbsolut = "/home/folder" + bc2.nama;
+  bc2.setPathAbsolut("/home/folder" + bc2.getNama());
   bc2.pasangElemen($("#panelBreadcrumb"));
   
   bc2.tandai();
