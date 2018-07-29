43e7b5fc148f60fc629044ff7361f26de0b0d6ed
Yoga Budi Yulianto <yogabudiyulianto@gmail.com>
2018-07-29 17:53:47 +0700

    pembuangan fungsi yang tidak terpakai

--- src/ui/js/fungsi_pembantu.js
+++ src/ui/js/fungsi_pembantu.js
@@ -52,34 +52,3 @@ function resetPanelPencarian() {
   $("#tanggalDibuat").show();
   $("#tanggalModif").show();
 }
-
-// method ini untuk tab file system
-function buatTabBaru(id, labelTab) {
-  $("#kumpulanTabFileSystem li").children().removeClass("active");
-  
-  var htmlTab = "<li id='tabFile_" + id + "' class='tab'>" +
-              "<a href='#konten_" + id + "' class='active' " +
-               "style='text-transform: none;'>" +
-               labelTab +
-               "</a>" +
-               "</li>";
-       
-  $("#kumpulanTabFileSystem").append(htmlTab);
-}
-
-function buatKontenKosong(idTab) {
-  var htmlDiv = "<div id='konten_" + idTab + "' class='tempatBerkas'></div>";
-  $("#panelFile").append(htmlDiv);
-}
-
-function pilihTab(idTab) {
-//  var instanceTab = M.Tabs.getInstance(
-//          document.getElementById("kumpulanTabFileSystem"));
-//  instanceTab.select("tabFile_" + idTab);
-//  instanceTab.updateTabIndicator();
-
-  var id = "konten_" + idTab;
-
-  $("#kumpulanTabFileSystem").tabs("select", id);
-  $("#kumpulanTabFileSystem").tabs("updateTabIndicator");
-}
